90c273b5ac51324ca73d1250748e4b4e4a83f225
Ben Thompson <ben@thompsondigital.com.au>
2018-12-15 13:25:24 +1000

    Elaborated on message processing. Completely untested but theoretically complete.

--- library/Models/MictrackBeacon.cs
+++ library/Models/MictrackBeacon.cs
@@ -1,8 +1,58 @@
+using System;
+
 namespace InvertedTomato.IO.Mictrack.Models
 {
     public class MictrackBeacon
     {
+        public String IMEI { get; set; } // 15 digits
+        public String GPRSUsername { get; set; }
+        public String GPRSPassword { get; set; }
+        public Events Event { get; set; }
+        public String BaseIdentifier { get; set; }
+        public DateTime At { get; set; }
+        public Statuses Status { get; set; }
+        public Double Latitude { get; set; }
+        public LatitudeIndicators LatitudeIndicator { get; set; }
+        public Double Longitiude { get; set; }
+        public LongitudeIndicators LongitiudeIndicator { get; set; }
+        public Double GroundSpeed { get; set; } // Knots
+        public Double Bearing { get; set; } // Degrees
 
+        public enum Events : Byte
+        {
+            None,
+            PowerSaveStopped,
+            PowerSaveMoving,
+            Call,
+            Disconnect,
+            HighTemperature,
+            InternalBatteryLow,
+            ExternalBatteryLow,
+            GeoFenceEnter,
+            GeoFenceExit,
+            SpeedLimitOver,
+            SpeedLimitUnder
+        }
+
+        public enum Statuses : Byte
+        {
+            Valid,
+            Invalid
+        }
+
+        public enum LatitudeIndicators : Byte
+        {
+            North,
+            South
+        }
+
+        public enum LongitudeIndicators : Byte
+        {
+            East,
+            West
+        }
     }
+
+
 
 }
--- library/Models/OnBeaconEventArgs.cs
+++ library/Models/OnBeaconEventArgs.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace InvertedTomato.IO.Mictrack.Models
 {
     public class OnBeaconEventArgs
     {
+        public String RemoteAddressString { get; set; }
+
         public MictrackBeacon Beacon { get; set; }
     }
 }
